6ab9bfac91948d003e8dfcc6562720738afb2c99
Patrick Fischer <fischerpl18@gmail.com>
2016-10-07 23:33:53 -0500

    Added placeholder settings.php page.

---
+++ settings.php
@@ -0,0 +1,79 @@
+<?php
+ob_start();
+session_start();
+require_once 'dbconnect.php';
+
+// if session is not set this will redirect to login page
+if( !isset($_SESSION['user']) ) {
+  header("Location: Main.php");
+  exit;
+}
+// select loggedin users detail
+if(isset($_SESSION['user'])) {
+  $res=mysql_query("SELECT * FROM Users WHERE idUsers=".$_SESSION['user']);
+  $userRow=mysql_fetch_array($res);
+}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Homepage</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+  <nav class="navbar navbar-inverse">
+    <div class="container">
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="#">Brentwood Leadership</a>
+      </div>
+      <div id="navbar" class="navbar-collapse collapse">
+        <ul class="nav navbar-nav">
+          <li><a href="/Main.php">Home</a></li>
+          <li><a href="/calendar.html">Calendar</a></li>
+          <li><a href="/contact.html">Contact Us</a></li>
+          <li><a href="/donate.html">Donate</a></li>
+          <li><a href="/login.php">Login</a></li>
+          <li><a href="/register.php">Register</a></li>
+        </ul>
+        <?php if(isset($_SESSION['user'])): ?>
+          <ul class="nav navbar-nav navbar-right">
+
+            <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+                <span class="glyphicon glyphicon-user"></span>&nbsp;Hi <?php echo $userRow['email']; ?>&nbsp;<span class="caret"></span></a>
+                <ul class="dropdown-menu">
+                  <li><a href="settings.php"><span class="glyphicon glyphicon-wrench"></span>&nbsp;Settings</a></li>
+                  <li><a href="logout.php?logout"><span class="glyphicon glyphicon-log-out"></span>&nbsp;Sign Out</a></li>
+                </ul>
+              </li>
+            </ul>
+          <?php endif; ?>
+        </div><!--/.nav-collapse -->
+      </div>
+    </nav>
+    <div class="container">
+      <div class="jumbotron">
+        <h1>Placeholder for settings page</h1>
+      </div>
+    </div>
+
+
+
+
+    <script src="assets/jquery-1.11.3-jquery.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+
+  </body>
+  </html>
+  <?php ob_end_flush(); ?>
