e83f3a2ad96571b6cd192b275d2e01e20f839376
Patrick Fischer <fischerpl18@gmail.com>
2016-10-08 11:45:51 -0500

    Added password change functionality.

--- settings.php
+++ settings.php
@@ -3,7 +3,7 @@ ob_start();
 session_start();
 require_once 'dbconnect.php';
 
-// if session is not set this will redirect to login page
+// if session is not set this will redirect to main page
 if( !isset($_SESSION['user']) ) {
   header("Location: Main.php");
   exit;
@@ -12,6 +12,66 @@ if( !isset($_SESSION['user']) ) {
 if(isset($_SESSION['user'])) {
   $res=mysql_query("SELECT * FROM Users WHERE idUsers=".$_SESSION['user']);
   $userRow=mysql_fetch_array($res);
+}
+
+// Allow the user to change their password
+if(isset($_POST['btn-change'])) {
+  // Get user details
+  $name = $userRow['username'];
+  $email = $userRow['email'];
+
+  // Clean up old password input to prevent SQL injections
+  $oldPass = trim($_POST['pwd']);
+  $oldPass = strip_tags($oldPass);
+  $oldPass = htmlspecialchars($oldPass);
+
+  // Clean up new password input to prevent SQL injections
+  $newPass = trim($_POST['newPwd']);
+  $newPass = strip_tags($newPass);
+  $newPass = htmlspecialchars($newPass);
+
+  // Check if password fields are empty
+  if(empty($oldPass)) {
+    $error = true;
+    $oldError = "Please enter your old password.";
+  }
+  if(empty($newPass)) {
+    $error = true;
+    $newError = "Please enter your new password.";
+  }
+  else if(strlen($newPass) < 8) {
+    $error = true;
+    $newError = "Password must be have at least 8 characters.";
+  }
+
+  // Encrypt both old and new password with SHA256().
+  $oldPass = hash('sha256', $oldPass);
+  $newPass = hash('sha256', $newPass);
+
+  // Make sure old password is correct.
+  $query = "SELECT id FROM Users where username='$name' AND email='$email' AND password='$oldPass'";
+  $res = mysql_query($query);
+  if($count == 0) {
+    $error = true;
+    $oldError = "Password incorrect.";
+  }
+  // If there is no error change the password.
+  if(!$error) {
+    $query = "UPDATE Users SET password='$newPass' WHERE email='$email'";
+    $res = mysql_query($query);
+    if($res) {
+      $errTyp = "success";
+      $errMSG = "Changed password.";
+      unset($name);
+      unset($email);
+      unset($oldPass);
+      unset($newPass);
+    }
+    else {
+      $errTyp = "danger";
+      $errMSG = "Something went wrong, try again later...";
+    }
+  }
 }
 ?>
 <!DOCTYPE html>
@@ -62,17 +122,17 @@ if(isset($_SESSION['user'])) {
         </div><!--/.nav-collapse -->
       </div>
     </nav>
-    <div class="container">
-      <p>Change password:</p>
+    <form>
       <div class="form-group">
-        <label for="pwd">Current Password:</label>
+        <label for="pwd">Password:</label>
         <input type="password" class="form-control" id="pwd">
       </div>
       <div class="form-group">
         <label for="newPwd">New Password:</label>
-        <input type="password" class="form-control" id="pwd">
+        <input type="password" class="form-control" id="newPwd">
       </div>
-    </div>
+      <button type="submit" class="btn btn-default" name="btn-change">Submit</button>
+    </form>
 
 
 
