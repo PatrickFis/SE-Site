e270c9ef960a0e21de9ff708445f0ea201a938b2
Patrick Fischer <fischerpl@mail.lipscomb.edu>
2016-10-19 10:53:04 -0500

    Fixed admin.php. Just need to add functionality.

--- admin.php
+++ admin.php
@@ -8,7 +8,7 @@ include_once 'dbconnect.php';
 $error = false;
 $res=mysql_query("SELECT * FROM Users WHERE idUsers=".$_SESSION['user']);
 $userRow=mysql_fetch_array($res);
-$adminRes=mysql_query("SELECT * FROM admin WHERE idadmin="$userRow['idUsers']);
+$adminRes=mysql_query("SELECT * FROM admin WHERE idadmin=".$userRow['idUsers']);
 $adminRow=mysql_fetch_array($adminRes); // Check to see if current user is an admin
 if($adminRow['idadmin'] == "") {
   header("Location: Main.php");
