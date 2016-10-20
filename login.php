ab6c11b74c196248185b017c4287c71615f6077d
PatrickFis <fischerpl@mail.lipscomb.edu>
2016-10-19 21:11:43 -0500

    Working on login page.

--- login.php
+++ login.php
@@ -1,17 +1,17 @@
 <?php
- ob_start();
- session_start();
- require_once 'dbconnect.php';
+ob_start();
+session_start();
+require_once 'dbconnect.php';
 
- // it will never let you open index(login) page if session is set
- if ( isset($_SESSION['user'])!="" ) {
+// it will never let you open index(login) page if session is set
+if ( isset($_SESSION['user'])!="" ) {
   header("Location: Main.php");
   exit;
- }
+}
 
- $error = false;
+$error = false;
 
- if( isset($_POST['btn-login']) ) {
+if( isset($_POST['btn-login']) ) {
 
   // prevent sql injections/ clear user invalid inputs
   $email = trim($_POST['email']);
@@ -24,40 +24,40 @@
   // prevent sql injections / clear user invalid inputs
 
   if(empty($email)){
-   $error = true;
-   $emailError = "Please enter your email address.";
+    $error = true;
+    $emailError = "Please enter your email address.";
   } else if ( !filter_var($email,FILTER_VALIDATE_EMAIL) ) {
-   $error = true;
-   $emailError = "Please enter valid email address.";
+    $error = true;
+    $emailError = "Please enter valid email address.";
   }
 
   if(empty($pass)){
-   $error = true;
-   $passError = "Please enter your password.";
+    $error = true;
+    $passError = "Please enter your password.";
   }
 
   // if there's no error, continue to login
   if (!$error) {
 
-   $password = hash('sha256', $pass); // password hashing using SHA256
+    $password = hash('sha256', $pass); // password hashing using SHA256
 
-   $res=mysql_query("SELECT idUsers, username, password FROM Users WHERE email ='$email'");
-   $row=mysql_fetch_array($res);
-   $count = mysql_num_rows($res); // if uname/pass correct it returns must be 1 row
+    $res=mysql_query("SELECT idUsers, username, password FROM Users WHERE email ='$email'");
+    $row=mysql_fetch_array($res);
+    $count = mysql_num_rows($res); // if uname/pass correct it returns must be 1 row
 
-   if( $count == 1 && $row['password']==$password ) {
-    $_SESSION['user'] = $row['idUsers'];
-    header("Location: Main.php");
-   } else {
-    $errMSG = "Incorrect Credentials, Try again...";
-   }
+    if( $count == 1 && $row['password']==$password ) {
+      $_SESSION['user'] = $row['idUsers'];
+      header("Location: Main.php");
+    } else {
+      $errMSG = "Incorrect Credentials, Try again...";
+    }
 
   }
 
- }
+}
 ?>
 <script>
-  function onSignIn(googleUser) {
+function onSignIn(googleUser) {
   var profile = googleUser.getBasicProfile();
   console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
   console.log('Name: ' + profile.getName());
@@ -80,88 +80,88 @@
 <body>
   <?php include_once("analyticstracking.php") ?>
 
-<nav class="navbar navbar-inverse">
-  <div class="container-fluid">
-    <div class="navbar-header">
-      <a class="navbar-brand" href="#">Brentwood Leadership</a>
+  <nav class="navbar navbar-inverse">
+    <div class="container-fluid">
+      <div class="navbar-header">
+        <a class="navbar-brand" href="#">Brentwood Leadership</a>
+      </div>
+      <ul class="nav navbar-nav">
+        <li><a href="/Main.php">Home</a></li>
+        <li><a href="/calendar.php">Calendar</a></li>
+        <li><a href="/contact.php">Contact Us</a></li>
+        <li><a href="/donate.php">Donate</a></li>
+        <li class="active"><a href="/login.php">Login</a></li>
+        <li><a href="/register.php">Register</a></li>
+      </ul>
     </div>
-    <ul class="nav navbar-nav">
-      <li><a href="/Main.php">Home</a></li>
-      <li><a href="/calendar.php">Calendar</a></li>
-      <li><a href="/contact.php">Contact Us</a></li>
-      <li><a href="/donate.php">Donate</a></li>
-      <li class="active"><a href="/login.php">Login</a></li>
-      <li><a href="/register.php">Register</a></li>
-    </ul>
-  </div>
-</nav>
+  </nav>
 
-<div class="container">
+  <div class="container">
 
- <div id="login-form">
-    <form method="post" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" autocomplete="off">
+    <div id="login-form">
+      <form method="post" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" autocomplete="off">
 
-     <div class="col-md-12">
+        <div class="col-md-12">
 
-         <div class="form-group">
-             <h2 class="">Sign In.</h2>
+          <div class="form-group">
+            <h2 class="">Sign In.</h2>
+          </div>
+
+          <div class="form-group">
+            <hr />
+          </div>
+
+          <?php
+          if ( isset($errMSG) ) {
+
+            ?>
+            <div class="form-group">
+              <div class="alert alert-danger">
+                <span class="glyphicon glyphicon-info-sign"></span> <?php echo $errMSG; ?>
+              </div>
             </div>
-
-         <div class="form-group">
-             <hr />
-            </div>
-
             <?php
-   if ( isset($errMSG) ) {
+          }
+          ?>
 
-    ?>
-    <div class="form-group">
-             <div class="alert alert-danger">
-    <span class="glyphicon glyphicon-info-sign"></span> <?php echo $errMSG; ?>
-                </div>
-             </div>
-                <?php
-   }
-   ?>
+          <div class="form-group">
+            <div class="input-group">
+              <span class="input-group-addon"><span class="glyphicon glyphicon-envelope"></span></span>
+              <input type="email" name="email" class="form-control" placeholder="Your Email" value="<?php echo $email; ?>" maxlength="40" />
+            </div>
+            <span class="text-danger"><?php echo $emailError; ?></span>
+          </div>
 
-            <div class="form-group">
-             <div class="input-group">
-                <span class="input-group-addon"><span class="glyphicon glyphicon-envelope"></span></span>
-             <input type="email" name="email" class="form-control" placeholder="Your Email" value="<?php echo $email; ?>" maxlength="40" />
-                </div>
-                <span class="text-danger"><?php echo $emailError; ?></span>
+          <div class="form-group">
+            <div class="input-group">
+              <span class="input-group-addon"><span class="glyphicon glyphicon-lock"></span></span>
+              <input type="password" name="pass" class="form-control" placeholder="Your Password" maxlength="15" />
             </div>
+            <span class="text-danger"><?php echo $passError; ?></span>
+          </div>
 
-            <div class="form-group">
-             <div class="input-group">
-                <span class="input-group-addon"><span class="glyphicon glyphicon-lock"></span></span>
-             <input type="password" name="pass" class="form-control" placeholder="Your Password" maxlength="15" />
-                </div>
-                <span class="text-danger"><?php echo $passError; ?></span>
-            </div>
+          <div class="form-group">
+            <hr />
+          </div>
 
-            <div class="form-group">
-             <hr />
-            </div>
+          <div class="form-group">
+            <button type="submit" class="btn btn-block btn-primary" name="btn-login">Sign In</button>
+          </div>
 
-            <div class="form-group">
-             <button type="submit" class="btn btn-block btn-primary" name="btn-login">Sign In</button>
-            </div>
-
-            <div class="form-group">
-             <hr />
-            </div>
-            <div class="g-signin2" data-onsuccess="onSignIn"></div>
-            <div class="form-group">
-             <a href="register.php">Sign Up Here...</a>
-            </div>
+          <div class="form-group">
+            <hr />
+          </div>
+          <div class="g-signin2" data-onsuccess="onSignIn"></div>
+          <div class="form-group">
+            <a href="register.php">Sign Up Here...</a>
+          </div>
 
         </div>
 
-    </form>
+      </form>
     </div>
 
-</div>
+  </div>
 
 </body>
 </html>
