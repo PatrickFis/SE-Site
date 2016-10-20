481938916798e4a7527bc90187a65c8e72a1f9ce
PatrickFis <fischerpl@mail.lipscomb.edu>
2016-10-19 19:59:24 -0500

    Working on logout.php.

--- logout.php
+++ logout.php
@@ -5,7 +5,7 @@
  } else if(isset($_SESSION['user'])!="") {
   header("Location: Main.php");
  }
- 
+
  if (isset($_GET['logout'])) {
   unset($_SESSION['user']);
   session_unset();
@@ -13,3 +13,13 @@
   header("Location: Main.php");
   exit;
  }
+?>
+
+<script>
+  function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+      console.log('User signed out.');
+    });
+  }
+</script>
