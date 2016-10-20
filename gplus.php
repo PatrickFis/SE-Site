4c6faf21c6bb148dabc1f86ec5c7431b9e494ea2
PatrickFis <fischerpl@mail.lipscomb.edu>
2016-10-19 21:29:30 -0500

    Working on login.

--- gplus.php
+++ gplus.php
@@ -1,6 +1,6 @@
 <?php
   if($_REQUEST["id"]) {
-    $url = 'https://www.googleapis.com/oauth2/v3/tokeninfo?id_token';
+    $url = 'https://www.googleapis.com/oauth2/v3/tokeninfo?id_token=';
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, $url);
     curl_setopt($ch, CURLOPT_POST, $_REQUEST["id"]);
