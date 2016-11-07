1b559cb6562d28092d2b36d53932b080f9a8e9dc
Patrick Fischer <fischerpl@mail.lipscomb.edu>
2016-11-07 09:56:03 -0600

    Added test code for emails.

--- quickmail.php
+++ quickmail.php
@@ -1,7 +1,31 @@
-<?php
-$to = 'fischerpl@mail.lipscomb.edu.';
-$subject = 'This is subject';
-$message = 'This is body of email';
-$from = "From: FirstName LastName <no-reply@bwoodleadership.com>";
-mail($to,$subject,$message,$from);
-?>
+<html>
+   
+   <head>
+      <title>Sending HTML email using PHP</title>
+   </head>
+   
+   <body>
+      
+      <?php
+/*         $to = "fischerpl@mail.lipscomb.edu";
+         $subject = "This is subject";
+         
+         $message = "<b>This is HTML message.</b>";
+         $message .= "<h1>This is headline.</h1>";
+         
+         $header = "From:no-reply@bwood.com \r\n";
+         $header .= "Cc:no-reply@bwood.com \r\n";
+         $header .= "MIME-Version: 1.0\r\n";
+         $header .= "Content-type: text/html\r\n";
+         
+         $retval = mail ($to,$subject,$message,$header);
+         
+         if( $retval == true ) {
+            echo "Message sent successfully...";
+         }else {
+            echo "Message could not be sent...";
+         }
+*/      ?>
+      
+   </body>
+</html>
