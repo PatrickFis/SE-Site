5d2ece04f774acc18ae399d238d3cc0eee422586
Patrick Fischer <fischerpl18@gmail.com>
2016-11-09 09:46:11 -0600

    Still testing.

--- forgottest.php
+++ forgottest.php
@@ -55,30 +55,18 @@ if( isset($_POST['btn-login']) ) {
   <nav class="navbar navbar-inverse">
     <div class="container">
       <div class="navbar-header">
-        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-          <span class="sr-only">Toggle navigation</span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-        </button>
         <a class="navbar-brand" href="#">Brentwood Leadership</a>
       </div>
-      <div id="navbar" class="navbar-collapse collapse">
-        <ul class="nav navbar-nav">
-          <li class="active"><a href="Main.php">Home</a></li>
-          <li><a href="calendar.php">Calendar</a></li>
-          <li><a href="contact.php">Contact Us</a></li>
-          <li><a href="donate.php">Donate</a></li>
-          <?php if(!isset($_SESSION['user'])): ?> <!-- Hides these two buttons if logged in. -->
-          <li><a href="login.php">Login</a></li>
-          <li><a href="register.php">Register</a></li>
-          <?php if($adminRow['isadmin'] == 1): ?>
-            <li><a href="admin.php">Admin</a></li>
-          <?php endif; ?>
-        <?php endif; ?>
-
-    <script src="assets/jquery-1.11.3-jquery.min.js"></script>
-    <script src="assets/js/bootstrap.min.js"></script>
+      <ul class="nav navbar-nav">
+        <li><a href="Main.php">Home</a></li>
+        <li><a href="calendar.php">Calendar</a></li>
+        <li><a href="contact.php">Contact Us</a></li>
+        <li><a href="donate.php">Donate</a></li>
+        <li class="active"><a href="login.php">Login</a></li>
+        <li><a href="register.php">Register</a></li>
+      </ul>
+    </div>
+  </nav>
 
     <div class="container">
 
