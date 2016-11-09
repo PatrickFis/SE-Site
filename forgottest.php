053ca4447dad361e915422226c1704a63f31ba21
Patrick Fischer <fischerpl18@gmail.com>
2016-11-09 09:39:31 -0600

    Testing a forgot password script.

---
+++ forgottest.php
@@ -0,0 +1,54 @@
+<?php
+ob_start();
+session_start();
+require_once 'dbconnect.php';
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Homepage</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+  <?php include_once("analyticstracking.php") ?>
+
+  <nav class="navbar navbar-inverse">
+    <div class="container">
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="#">Brentwood Leadership</a>
+      </div>
+      <div id="navbar" class="navbar-collapse collapse">
+        <ul class="nav navbar-nav">
+          <li class="active"><a href="Main.php">Home</a></li>
+          <li><a href="calendar.php">Calendar</a></li>
+          <li><a href="contact.php">Contact Us</a></li>
+          <li><a href="donate.php">Donate</a></li>
+          <?php if(!isset($_SESSION['user'])): ?> <!-- Hides these two buttons if logged in. -->
+          <li><a href="login.php">Login</a></li>
+          <li><a href="register.php">Register</a></li>
+          <?php if($adminRow['isadmin'] == 1): ?>
+            <li><a href="admin.php">Admin</a></li>
+          <?php endif; ?>
+        <?php endif; ?>
+
+
+
+
+    <script src="assets/jquery-1.11.3-jquery.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+
+  </body>
+  </html>
+  <?php ob_end_flush(); ?>
