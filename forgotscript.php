a27cfa7a7afd1e5b28671a3b6c30a53bed933379
Patrick Fischer <fischerpl18@gmail.com>
2016-11-15 19:54:28 -0600

    Test commit for forgot password script.

---
+++ forgotscript.php
@@ -0,0 +1,14 @@
+<?php
+ob_start();
+session_start();
+require_once 'dbconnect.php';
+require("/var/www/html/vendor/phpmailer/phpmailer/PHPMailerAutoload.php");
+// If user is currently logged in leave this page.
+if ( isset($_SESSION['user'])!="" ) {
+  header("Location: Main.php");
+  exit;
+}
+
+$email = trim($_POST['email']);
+echo "HELLO.";
+?>
