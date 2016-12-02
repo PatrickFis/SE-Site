435a5fccb74b4d601afabd1cbb289ef5df9c9aa5
Patrick Fischer <fischerpl18@gmail.com>
2016-12-02 10:40:48 -0600

    Testing sponsorDeleter

--- adminScripts/sponsorDeleter.php
+++ adminScripts/sponsorDeleter.php
@@ -1,3 +1,15 @@
 <?php
-  
+  include_once ('../dbconnect.php');
+  $deleteme = $_POST['selImg'];
+  echo $deleteme;
+  // Get the image path from the database
+  $selectQuery = "SELECT * FROM sponsors WHERE sidebarName = $deleteme";
+  $res = mysql_query($selectQuery);
+  // Delete the file from the sponsorimg directory
+  while($row = mysql_fetch_array($res)) {
+    unlink($res['imgpath']);
+  }
+  // Query to delete sponsor
+  $deleteQuery = "DELETE FROM sponsors WHERE sidebarName = $deleteme";
+  $res = mysql_query($deleteQuery);
  ?>
