d5ba020238f5999b633648bf2874ccf6e5fc9a6c
Patrick Fischer <fischerpl18@gmail.com>
2016-12-02 12:58:25 -0600

    Changed title on calendar page

--- calendar.php
+++ calendar.php
@@ -19,7 +19,7 @@ if(isset($_SESSION['user'])) {
 <!DOCTYPE html>
 <html>
 <head>
-  <title>Homepage</title>
+  <title>Calendar</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
