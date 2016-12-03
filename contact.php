495bfc0aad1f85267dead3c689374b8441f615b3
Bryce Skelton <mbskelton@mail.lipscomb.edu>
2016-12-02 23:47:24 -0600

    test

--- contact.php
+++ contact.php
@@ -140,16 +140,6 @@ background-color: #000000;
           <div class="item">
             <img src="img/leadership_shirt.jpg" alt="shirt" width="460" height="345">
           </div> -->
-        </div> <!-- left and right sliders for carousel -->
-        <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
-          <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-          <span class="sr-only">Previous</span>
-        </a>
-        <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
-          <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-          <span class="sr-only">Next</span>
-        </a>
-      </div>
 
       </div>
     </div>
