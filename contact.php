a2dfeec84678ad179f2e22360580e7e6db40ad78
Bryce Skelton <mbskelton@mail.lipscomb.edu>
2016-12-02 23:20:12 -0600

    add directional controls for carousel

--- contact.php
+++ contact.php
@@ -140,7 +140,16 @@ background-color: #000000;
           <div class="item">
             <img src="img/leadership_shirt.jpg" alt="shirt" width="460" height="345">
           </div> -->
-        </div>
+        </div> <!-- left and right sliders for carousel -->
+        <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+          <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+          <span class="sr-only">Previous</span>
+        </a>
+        <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+          <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+          <span class="sr-only">Next</span>
+        </a>
+      </div>
 
       </div>
     </div>
