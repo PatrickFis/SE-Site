11d0cd6f0b19de29afb817c76dbb3cdfe735119e
Patrick Fischer <fischerpl18@gmail.com>
2016-12-04 17:38:00 -0600

    Added line breaks on mobile.

--- Main.php
+++ Main.php
@@ -146,6 +146,9 @@ if(isset($_SESSION['user'])) {
            ?>
         </div>
       </div>
+      <div class="visible-lg visible-md hidden-sm hidden-xs">
+        <br><br><br><br><br>
+      </div>
     </div>
     <div class="visible-lg visible-md hidden-sm hidden-xs">
       <!-- Hide this on small devices, they can look at the calendar page. -->
