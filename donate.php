f14425243966b874d471c709d7fc46da3b201191
Patrick Fischer <fischerpl18@gmail.com>
2016-12-04 13:12:02 -0600

    Added a demo PayPal button

--- donate.php
+++ donate.php
@@ -137,6 +137,10 @@ if(isset($_SESSION['user'])) {
           <button type="button" class="btn btn-success">Pay Now</button>
         </div>
       </div>
+      <div clas="form-group">
+        <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+        <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+      </div>
     </fieldset>
   </form>
 </div>
