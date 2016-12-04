8e065fc8f3174f49dbf1f8a28f4d7301bbc6792b
Patrick Fischer <fischerpl18@gmail.com>
2016-12-04 16:05:26 -0600

    Fixed an error in the class deleter SQL query

--- adminScripts/classDeleter.php
+++ adminScripts/classDeleter.php
@@ -2,7 +2,7 @@
   include_once ('../dbconnect.php');
   $deleteme = $_POST['selCls'];
   // Query to delete class member
-  $deleteQuery = "DELETE FROM classMembers WHERE sidebarName = '$deleteme'";
+  $deleteQuery = "DELETE FROM classMembers WHERE name = '$deleteme'";
   $res = mysql_query($deleteQuery);
   // Redirect the user back to the admin page.
   header("Location: ../admin.php");
